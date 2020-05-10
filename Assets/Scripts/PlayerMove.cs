ee99c0d3c476591b9941470335cfdd747fdc7812
Kengo Kashihara <KengoKashihara1998@gmail.com>
2020-05-11 00:53:17 +0900

    プレイヤーの前進を実装
    上キーの入力でプレイヤーが前進するように実装しました

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] private float speed = 0.0f;
+    [SerializeField] private float MaxSpeed = 0.0f;
+    [SerializeField] private float turnSpeed = 0.0f;
+
+    private Rigidbody rigid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void TurnAround(float speed)
+    {
+        float y = transform.eulerAngles.y + speed * Time.deltaTime;
+        Quaternion rot = Quaternion.Euler(0.0f, y, 0.0f);
+        rigid.rotation = rot;
+    }
+
+    public void MoveForward()
+    {
+        if (rigid.velocity.z >= MaxSpeed) return;
+        float angle = transform.eulerAngles.y;
+
+        // -Y方向へ回転
+        if (angle > 1.0f && angle < 180.0f)
+        {
+            TurnAround(-turnSpeed);
+        }
+        // Y方向へ回転
+        if(angle >= 180.0f && angle < 359.0f)
+        {
+            TurnAround(turnSpeed);
+        }
+        // 前進
+        if(angle >= 359.0f || angle <= 1.0f)
+        {
+            rigid.rotation = Quaternion.AngleAxis(0.0f, Vector3.up);
+            rigid.AddForce(Vector3.forward * speed);
+        }
+        // 角度がマイナスになった場合の対処
+        if(angle < 0)
+        {
+            rigid.rotation = Quaternion.AngleAxis(0.0f, Vector3.up);
+        }
+        
+    }
+}
