f56f6c8404587e85cc5b0a17ffbdf873a7d22553
Kengo Kashihara <KengoKashihara1998@gmail.com>
2020-05-12 22:18:51 +0900

    移動のスクリプトを作成
    移動のスクリプトを作成しました

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private PlayerMove move;
+    // Start is called before the first frame update
+    void Start()
+    {
+        move = GetComponent<PlayerMove>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// 移動
+    /// </summary>
+    /// <param name="dir">移動する方向</param>
+    public void Movement(Vector3 dir)
+    {
+
+    }
+}
