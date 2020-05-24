638bec44c192d989221999b0cbc0098ea4c2849a
Kengo Kashihara <KengoKashihara1998@gmail.com>
2020-05-24 23:51:09 +0900

    カードの情報を追加
    カードの柄や数字などの情報を追加しました

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -2,12 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum Number
+{
+    Ace   = 1,
+    Two   = 2,
+    Three = 3,
+    Four  = 4,
+    Five  = 5,
+    Six   = 6,
+    Seven = 7,
+    Eight = 8,
+    Nine  = 9,
+    Ten   = 10,
+    Jack  = 11,
+    Queen = 12,
+    King  = 13,
+}
+
+public enum Suit
+{
+    Spade,
+    Heart,
+    Club,
+    Diamond,
+}
+
+public struct CardData
+{
+    public Suit suit;
+    public Number number;
+}
+
 public class Card : MonoBehaviour
 {
+    private CardData data;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        data.suit   = Suit.Spade;
+        data.number = Number.Ace;
     }
 
     // Update is called once per frame
