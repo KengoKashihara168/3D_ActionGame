702ee913fcc3a41b75d5d1afa3ddb8be120c1bb3
KengoKashihara <KengoKashihara1998@gmail.com>
2020-08-05 23:55:06 +0900

    タイトルシーンのサウンドを設定
    タイトルシーンにサウンドを設定しました

--- Assets/Scripts/TitleScene.cs
+++ Assets/Scripts/TitleScene.cs
@@ -5,20 +5,55 @@ using UnityEngine.SceneManagement;
 
 public class TitleScene : MonoBehaviour
 {
-    private readonly string GameScene = "GameScene";
+    // 定数
+    private readonly string NextScene = "GameScene"; // 次のシーン
+
+    // 外部設定変数
+    [SerializeField] private AudioClip soundEffect = null;  // SE
+    [SerializeField] private bool      isStopBGM   = false; // SE再生時にBGMを停止するフラグ
+
+    private SoundManager sound;     // サウンド
+    private bool         isPushKey; // キーが入力されたフラグ
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // サウンドの初期化
+        sound = GetComponent<SoundManager>();
+        sound.Initialize();
+        // ボタン押下フラグの初期化
+        isPushKey = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        // キーが押され、かつまだ押されていなければ
+        if(Input.GetKeyDown(KeyCode.Space) && isPushKey == false)
         {
-            SceneManager.LoadScene(GameScene);
+            isPushKey = true;
+            // SEの再生
+            sound.PlaySE(soundEffect, isStopBGM);
+        }
+
+        if(isPushKey)
+        {
+            // シーン遷移
+            ChangeScene();
+        }
+        
+    }
+
+    /// <summary>
+    /// シーン遷移
+    /// </summary>
+    private void ChangeScene()
+    {
+        // SEの再生が終了していたら
+        if(sound.IsEndSE(soundEffect.name))
+        {
+            SceneManager.LoadScene(NextScene);
         }
     }
+
 }
